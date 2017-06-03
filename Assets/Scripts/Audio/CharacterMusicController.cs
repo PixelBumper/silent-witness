c351b00759974a3586fe4c500fca7630c4d34037
Sergio R. Lumley <lumley256@gmail.com>
2017-06-04 00:43:53 +0200

    Fixed timing issues with music playing

--- Assets/Scripts/Audio/CharacterMusicController.cs
+++ Assets/Scripts/Audio/CharacterMusicController.cs
@@ -9,10 +9,8 @@ public class CharacterMusicController : MonoBehaviour
     private GameObject _distanceReference;
     private AudioSource _audioSource;
     private float _remainingPlayingTime;
-    private float _remainingSpacingTime;
-
+    private float _lastPlayingTime;
     private float _lastTempo;
-    private float _lastLoopSpacing;
 
     private Vector3 _previousPosition;
     private Vector3 _previousScale;
@@ -28,29 +26,24 @@ public class CharacterMusicController : MonoBehaviour
         _audioSource.loop = false;
         _audioSource.clip = _music;
         _previousPosition = transform.position;
+
+        _remainingPlayingTime = _music.length;
+        UpdateSoundProperties();
     }
 
     private void Update()
     {
         _remainingPlayingTime = Mathf.Max(_remainingPlayingTime - Time.deltaTime, 0f);
-        
+
         if (_previousPosition != transform.position || _previousScale != transform.localScale)
         {
             UpdateSoundProperties();
-            _previousPosition = transform.position;
-            _previousScale = transform.localScale;
         }
 
         if (_remainingPlayingTime <= 0)
         {
-            _remainingSpacingTime = Mathf.Max(_remainingSpacingTime - Time.deltaTime, 0f);
-
-            if (_remainingSpacingTime <= 0)
-            {
-                _remainingPlayingTime = _audioSource.clip.length;
-                _remainingSpacingTime = _lastTempo * _lastLoopSpacing;
-                _audioSource.Play();
-            }
+            _remainingPlayingTime = _lastPlayingTime;
+            _audioSource.Play();
         }
     }
 
@@ -58,27 +51,30 @@ public class CharacterMusicController : MonoBehaviour
     {
         var referencePosition = _distanceReference.transform.position;
         var position = transform.position;
-        _lastLoopSpacing = GetPercentageDistance(referencePosition.x, position.x);
-        var previousSpacing = GetPercentageDistance(referencePosition.x, _previousPosition.x);
-        var pitch = GetPercentageDistance(referencePosition.z, position.z);
+        var previousSpacingSeconds = _lastPlayingTime;
+        _lastPlayingTime = GetSpacingDistanceSeconds(referencePosition.x, position.x);
         _lastTempo = GetPercentageDistance(referencePosition.y, position.y);
-        var previousTempo = GetPercentageDistance(referencePosition.y, _previousPosition.y);
         var volumeScale = transform.localScale.x;
-
-        var tempoDifferenceMultiplier = 1f + previousTempo - _lastTempo;
-        _remainingPlayingTime *= tempoDifferenceMultiplier;
 
         _audioSource.pitch = _lastTempo;
         _audioSource.volume = volumeScale;
 
-        if (_remainingPlayingTime > 0)
+        _remainingPlayingTime -= previousSpacingSeconds - _lastPlayingTime; 
+        while (_remainingPlayingTime < 0)
         {
-            _remainingSpacingTime = _lastLoopSpacing;
+            _remainingPlayingTime += _lastPlayingTime;
         }
-        else
-        {
-            _remainingSpacingTime -= previousSpacing - _lastLoopSpacing;
-        }
+        
+        _previousPosition = transform.position;
+        _previousScale = transform.localScale;
+    }
+
+    private float GetSpacingDistanceSeconds(float referenceAxis, float actualAxis)
+    {
+        var actualDistance = Mathf.Abs(referenceAxis - actualAxis);
+        var adjustedDistance = Mathf.Max((actualDistance - 10f) / 10f + 1f, 0.1f);
+
+        return _music.length * adjustedDistance;
     }
 
     private float GetPercentageDistance(float referenceAxis, float actualAxis)
