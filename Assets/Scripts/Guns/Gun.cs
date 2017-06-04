36f202f39b7009ab5c566c48c3207c493aa7a7fd
Philippe Proulx <philippe.php@gmail.com>
2017-06-04 23:08:32 +0200

    Add gun selector

--- Assets/Scripts/Guns/Gun.cs
+++ Assets/Scripts/Guns/Gun.cs
@@ -13,4 +13,24 @@ public abstract class Gun : MonoBehaviour
 			Debug.LogError("Gun needs a Targeter component to work properly", this);
 		}
 	}
+
+	public void Enable()
+	{
+		enabled = true;
+		
+		if (_icon)
+		{
+			_icon.Enable();
+		}
+	}
+
+	public void Disable()
+	{
+		enabled = false;
+		
+		if (_icon)
+		{
+			_icon.Disable();
+		}
+	}
 }
---
+++ Assets/Scripts/Guns/GunSelector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSelector : MonoBehaviour
+{
+	[SerializeField] private List<Gun> _guns;
+	[SerializeField] private float _delay = .1f;
+	
+	private float _lastSelectTime;
+	private int _currentGunIndex;
+
+	private void Start()
+	{
+		_guns[0].Enable();
+		
+		for (var i = 1; i < _guns.Count; i++)
+		{
+			_guns[i].Disable();
+		}
+	}
+
+	private void Update()
+	{
+		if (Math.Abs(Input.mouseScrollDelta.y) > Mathf.Epsilon &&
+		    Time.time - _lastSelectTime >= _delay)
+		{
+			_lastSelectTime = Time.time;
+
+			var delta = -(int)Mathf.Sign(Input.mouseScrollDelta.y);
+			
+			_guns[_currentGunIndex].Disable();
+			
+			_currentGunIndex = (_currentGunIndex + _guns.Count + delta) % _guns.Count;
+
+			_guns[_currentGunIndex].Enable();
+		}
+	}
+}
