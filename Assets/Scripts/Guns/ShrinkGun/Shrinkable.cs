dede5baefa61351d9453f2741b8f98d0f2558324
Philippe Proulx <philippe.php@gmail.com>
2017-06-04 21:52:09 +0200

    Add ShrinkGun

---
+++ Assets/Scripts/Guns/ShrinkGun/Shrinkable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Shrinkable : MonoBehaviour, IShrinkable
+{
+	public void Shrink(float sizeDelta)
+	{
+		transform.localScale -= Vector3.one * sizeDelta;
+	}
+
+	public float Size
+	{
+		get { return transform.localScale.x; }
+	}
+}
