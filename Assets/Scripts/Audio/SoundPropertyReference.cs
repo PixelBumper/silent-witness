56d59d4f545f4f9e0a526704cc4ecab1137e008b
Sergio R. Lumley <lumley256@gmail.com>
2017-06-04 13:56:31 +0200

    Split sound property reference from character music controller, created audio testing scene

--- Assets/Scripts/Audio/SoundPropertyReference.cs
+++ Assets/Scripts/Audio/SoundPropertyReference.cs
@@ -1,7 +1,23 @@
-namespace DefaultNamespace.Audio
+using UnityEngine;
+
+public class SoundPropertyReference : MonoBehaviour
 {
-    public class SoundPropertyReference
+    [SerializeField] private float _spacingDistance = 10f;
+
+    public float GetSpacingSecondsMultiplier(Transform otherTransform)
     {
-        
+        var referenceAxis = transform.position.x;
+        var actualAxis = otherTransform.position.x;
+        var actualDistance = Mathf.Abs(referenceAxis - actualAxis);
+        var adjustedDistance = Mathf.Max((actualDistance - _spacingDistance) / _spacingDistance + 1f, 0.1f);
+
+        return adjustedDistance;
+    }
+
+    public float GetTempoPercentage(Transform otherTransform)
+    {
+        var referenceAxis = transform.position.y;
+        var actualAxis = otherTransform.position.y;
+        return Mathf.Abs(referenceAxis - actualAxis);
     }
 }
