d2272126833fd97c05e44128461ca5ef4b205146
Philippe Proulx <philippe.php@gmail.com>
2017-06-04 20:54:30 +0200

    Add CloneGun

---
+++ Assets/Scripts/Guns/CloneGun/CloneGun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CloneGun : Gun 
+{
+	[SerializeField] private int _inputMouseButton = 2;
+	
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(_inputMouseButton))
+		{
+			Fire();
+		}
+	}
+
+	private void Fire()
+	{
+		var targetedObject = Targeter.TargetedGameObject;
+		
+		if (targetedObject != null)
+		{
+			var clonable = targetedObject.GetComponent<IClonable>();
+
+			if (clonable != null)
+			{
+				clonable.Clone();
+			}
+		}
+	}
+}
