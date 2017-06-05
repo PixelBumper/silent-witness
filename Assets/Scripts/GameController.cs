d0c19843a41a83dbf4561365d2fadfd39c04bf30
pixelbumper <PixelBumper@users.noreply.github.com>
2017-06-05 12:45:50 +0200

    Added respawning

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+	public Vector3 SpawningPosition = new Vector3(0, 1, 0);
+
+	public float RespawningHeight = -10f;
+	
+	private GameObject _character;
+	// Use this for initialization
+	void Start ()
+	{
+		_character=GameObject.Find("Character");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_character.transform.position.y < -RespawningHeight)
+		{
+			_character.transform.position = SpawningPosition;
+		}
+	
+	}
+}
